fa25cf92bbc1ab94e334800ff617267bfabd910e
Denittka <dan.belokon@gmail.com>
2021-06-29 22:31:01 +0300

    trying cv2

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from Neural import *
+"""
+modulo 1 -- change a photo
+"""
 
-neural_network = NeuralNetwork()
-print(neural_network.first_layer)
+from os import listdir
+from os.path import abspath
+import cv2
+import numpy as np
+
+# for file_name in listdir("./workDirectory"):
+img = cv2.imread("./workDirectory/image_20.JPG")
+b, g, r = (np.array(a, int) for a in cv2.split(img))
+g = np.zeros(b.shape, np.int64)
+r = np.zeros(b.shape, np.int64)
+yellowBGR = cv2.merge([b, g, r])
+cv2.imwrite("yellow.jpg", yellowBGR)
+
+
+"""
+modulo 2 -- through neuralNetwork
+"""
