c5f9b814d4bd468ebefbf04a4ef820348f9fc9ca
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-06-29 14:01:27 +0300

    первая модель иди как

---
+++ ad/ddd.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras import utils
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import matplotlib.pyplot as plt
+
+batch_size=1
+image_size=(200, 200)
+
+# И да фотки я уменьшил до 200 на 200
+
+train_dataset = image_dataset_from_directory('ad\TESTIMAGES2',
+                                             subset='training',
+                                             seed=42,
+                                             validation_split=0.1,
+                                             batch_size=batch_size,
+                                             image_size=image_size)
+
+validation_dataset = image_dataset_from_directory('ad\TESTIMAGES2',
+                                             subset='validation',
+                                             seed=42,
+                                             validation_split=0.1,
+                                             batch_size=batch_size,
+                                             image_size=image_size)
+test_dataset = image_dataset_from_directory('ad\TESTIMAGES2',
+                                             batch_size=batch_size,
+                                             image_size=image_size)
+
+class_names = train_dataset.class_names
+print(class_names)
+
+
+
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+
+
+model = Sequential()
+
+model.add(Conv2D(16, (5, 5), padding='same', 
+                 input_shape=(200, 200, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (5, 5), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (5, 5), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (5, 5), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(2, activation='softmax'))
+
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer="adam",
+              metrics=['accuracy'])
+
+
+history = model.fit(train_dataset, 
+                    validation_data=validation_dataset,
+                    epochs=5,
+                    verbose=2)
+
+scores = model.evaluate(test_dataset, verbose=1)
+
+print("Доля верных ответов на тестовых данных, в процентах:", round(scores[1] * 100, 4))
+
+
+#рил прикольного что можно так сохраняль
+model.save("bears_model.h5")
+
+
+
---
+++ ad/loadmodel.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras import utils
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import matplotlib.pyplot as plt
+
+#тут хотел уже загружать обученную модель
+
+
+
