6695432d95da312e634c04f7092a82ce464ed903
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-06-30 19:30:33 +0300

    исправление

--- poisk.py
+++ poisk.py
@@ -1,28 +1,45 @@
 import cv2
 from os import listdir
+import numpy as np
 
-img = cv2.imread("./changed/withBears/" + "_2016-05-12 14-20-10_2145_2R.JPG")
+img = cv2.imread("ad\TESTIMAGES\withBears\\" + "_2016-04-25 11-06-03_2568_L.JPG")
+
+img = cv2.resize(img, (750, 750))
+
+imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype("float32")
+(h, s, v) = cv2.split(imghsv)
+s = s * 5.4
+s = np.clip(s, 0, 255)
+imghsv = cv2.merge([h, s, v])
+imgBGR = cv2.cvtColor(imghsv.astype("uint8"), cv2.COLOR_HSV2BGR)
+result = np.uint8(np.clip((-0.5 * imgBGR + 100), 0, 255))
 
 maxd = []
 
 for x in range(0,750):
     for y in range(0,750):
 
-        if img[x,y,0] > img[x,y,1] + 20 and img[x,y,0] > img[x,y,2] +20 :
+        if result[x,y,0] > result[x,y,1] + 30 and result[x,y,0] > result[x,y,2] +30 :
             img[x,y,0] = 255
             img[x,y,1] = 0
             img[x,y,2] = 0
             
-            maxd += [(y,x)]
+            maxd += [[y,x,1]]
 
-#x = int(dM10 / dArea)
-#y = int(dM01 / dArea)
-
-lastxy = (-100,-100)
 for i in maxd:
-    if ((i[0]-lastxy[0])**2+(i[0]-lastxy[0])**2)**0.5 > 5:
-        cv2.rectangle(img, (i[0]-10, i[1]-10),(i[0]+10, i[1]+10),(0,0,255), 1)
-        lastxy = i
+    for j in maxd:
+        if i != j:
+            d = ((i[0]-j[0])**2+(i[0]-j[0])**2)**0.5 or 1
+            if d < 5:
+                i[2] += 1/d
+
+
+print(maxd)
+lutsh = max(maxd,key=lambda i: i[2])
+
+
+cv2.rectangle(img, (lutsh[0]-10, lutsh[1]-10),(lutsh[0]+10, lutsh[1]+10),(0,0,255), 1)
+
 
 cv2.imshow("1",img)
 cv2.waitKey(0)
