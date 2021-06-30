3ae731944cdef190f1435a282a8e7335631a4a2b
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-06-30 17:42:44 +0300

    оп вот и нахождение медведя

--- poisk.py
+++ poisk.py
@@ -1,37 +1,28 @@
 import cv2
 from os import listdir
 
-img = cv2.imread("./changed/withBears/" + "2016-05-13 10-16-57_0024_1L.JPG")
+img = cv2.imread("./changed/withBears/" + "_2016-05-12 14-20-10_2145_2R.JPG")
 
-imgmoment = img.copy()
-
-maxd = (0,0)
+maxd = []
 
 for x in range(0,750):
     for y in range(0,750):
 
         if img[x,y,0] > img[x,y,1] + 20 and img[x,y,0] > img[x,y,2] +20 :
-            imgmoment[x,y,0] = 255
-            imgmoment[x,y,1] = 0
-            imgmoment[x,y,2] = 0
-
-            maxd = (x,y)
-cv2.imshow("2",imgmoment)
-imgmoment = cv2.cvtColor(imgmoment, cv2.COLOR_BGR2HSV )
-
-imgmoment = cv2.inRange(imgmoment, (220,0,0), (240,100,100))
-moments = cv2.moments(imgmoment, 1)
-
-print(moments.items())
-dM01 = moments['m01']
-dM10 = moments['m10']
-dArea = moments['m00']
-
+            img[x,y,0] = 255
+            img[x,y,1] = 0
+            img[x,y,2] = 0
+            
+            maxd += [(y,x)]
 
 #x = int(dM10 / dArea)
 #y = int(dM01 / dArea)
-cv2.rectangle(img, (maxd[0]-5, maxd[1]-5),(maxd[0]+5, maxd[1]+5),(0,0,255), 1)
-cv2.rectangle(img, (maxd[0]-5, maxd[1]-5),(maxd[0]+5, maxd[1]+5),(0,0,255), 1)
+
+lastxy = (-100,-100)
+for i in maxd:
+    if ((i[0]-lastxy[0])**2+(i[0]-lastxy[0])**2)**0.5 > 5:
+        cv2.rectangle(img, (i[0]-10, i[1]-10),(i[0]+10, i[1]+10),(0,0,255), 1)
+        lastxy = i
 
 cv2.imshow("1",img)
 cv2.waitKey(0)
